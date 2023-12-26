55ca2cd9bded6408afa1ed94d553f30b33d1a2cb
Marcelo José da Silva Santos <55773412+MarceloJSSantos@users.noreply.github.com>
2023-12-26 16:11:50 -0300

    Solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario (val nome: String, val dataNascimento: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Parabéns '${usuario.nome}', você está matriculado(a) na formação '$nome'!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //criando conteúdos educacionais (cursos)
+    val conteudoEducacional1 = ConteudoEducacional("Trabalhando em Equipes Ágeis", 180, Nivel.BASICO)
+    val conteudoEducacional2 = ConteudoEducacional("Versionamento de Código com Git e GitHubs", 120, Nivel.BASICO)
+    val conteudoEducacional3 = ConteudoEducacional("Tratamento de Exceções em Kotlin", 120, Nivel.AVANCADO)
+    
+    //criando lista de conteúdos educacionais
+    var listaConteudosEducacionais = listOf(conteudoEducacional1, conteudoEducacional2, conteudoEducacional3)
+        
+    //criando formação
+    val formacao = Formacao("Bootcamp Back-end Kotlin NTT DATA", listaConteudosEducacionais, Nivel.INTERMEDIARIO)
+    
+    //criando usuários
+    val usuario1 = Usuario(nome = "João", dataNascimento = "10/10/1996")
+    val usuario2 = Usuario(nome = "Maria", dataNascimento = "15/12/2000")
+    val usuario3 = Usuario(dataNascimento = "25/08/2003", nome = "Pedro")
+    val usuario4 = Usuario(dataNascimento = "25/03/1985", nome = "Júlia")
+    
+    //matriculando alunos nas formações
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+    formacao.matricular(usuario4)
+    
+   println("\nQuantidade de alunos matriculados na formação '${formacao.nome}': ${formacao.inscritos.count()}")
+   println("Quantidade de cursos da formação '${formacao.nome}': ${formacao.conteudos.count()}")
+   println("Carga horária da formação '${formacao.nome}': ${formacao.conteudos.sumOf { it.duracao }} minutos")
+   
 }
